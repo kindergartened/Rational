36a51bd0eb67a3430ad7b7855f2eed1667ef40e6
Shusha Poebusha <Shusha Poebusha@DESKTOP-U3K3TCO>
2023-10-30 20:28:48 +0300

    Tests Fix

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-using Lib;
+     using Lib;
 using static Lib.BigInt;
 using static Lib.Utils;
 namespace Tests
@@ -13,9 +13,8 @@ namespace Tests
             string n2 = "885495849589458945849584958489588";
             BigInt num1 = new BigInt(n1);
             BigInt num2 = new BigInt(n2);
-            BigInt result = new BigInt("0");
-            result = num1 * num2;
-            Assert.AreEqual(MultiplyBig(n1,n2), result.ToString());
+            BigInt result = num1 * num2;
+            Assert.AreEqual(TestMultiplyBig(n1,n2), result.ToString());
         }
         [TestMethod]
         public void SumBig()
@@ -24,9 +23,8 @@ namespace Tests
             string n2 = "885495849589458945849584958489588";
             BigInt num1 = new BigInt(n1);
             BigInt num2 = new BigInt(n2);
-            BigInt result = new BigInt("0");
-            result = num1 + num2;
-            Assert.AreEqual(AddBig(n1, n2), result.ToString());
+            BigInt result = num1 + num2;
+            Assert.AreEqual(TestAddBig(n1, n2), result.ToString());
         }
         [TestMethod]
         public void SubBig()
@@ -35,21 +33,18 @@ namespace Tests
             string n1 = "885495849589458945849584958489588";
             BigInt num1 = new BigInt(n1);
             BigInt num2 = new BigInt(n2);
-            BigInt result = new BigInt("0");
-            result = num1 - num2;
-            Assert.AreEqual(MinusBig(n1, n2), result.ToString());
+            BigInt result = num1 - num2;
+            Assert.AreEqual(TestMinusBig(n1, n2), result.ToString());
         }
         [TestMethod]
-        //дописать
         public void DivBig()
         {
             string n2 = "111111111111111111111111145454567";
             string n1 = "885495849589458945849584958489588";
             BigInt num1 = new BigInt(n1);
             BigInt num2 = new BigInt(n2);
-            BigInt result = new BigInt("0");
-            result = num1 /num2;
-            Assert.AreEqual(DivideBig(n1, n2), result.ToString());
+            BigInt result = num1 / num2;
+            Assert.AreEqual(TestDivideBig(n1, n2), result.ToString());
         }
     }
 }
