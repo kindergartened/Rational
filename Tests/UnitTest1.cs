c7cfb086ad94bdb0086b97f8c0303f2252197cc9
Dmitrich7 <pashokkalina@gmail.com>
2023-12-13 22:39:22 +0300

    тесты

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -17,6 +17,36 @@ namespace Tests
             Assert.AreEqual(TestMultiplyBig(n1,n2), result.ToString());
         }
         [TestMethod]
+        public void MultiplyTest1()
+        {
+            string n1 = "111111111111111111111111145454567";
+            string n2 = "0";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 * num2;
+            Assert.AreEqual(TestMultiplyBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void MultiplyTest2()
+        {
+            string n1 = "1";
+            string n2 = "885495849589458945849584958489588";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 * num2;
+            Assert.AreEqual(TestMultiplyBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void MultiplyTest3()
+        {
+            string n1 = "-1";
+            string n2 = "885495849589458945849584958489588";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 * num2;
+            Assert.AreEqual(TestMultiplyBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
         public void SumBigTest()
         {
             string n1 = "111111111111111111111111145454567";
@@ -27,7 +57,37 @@ namespace Tests
             Assert.AreEqual(TestAddBig(n1, n2), result.ToString());
         }
         [TestMethod]
+        public void SumBigTest1()
+        {
+            string n1 = "111111111111111111111111145454567";
+            string n2 = "0";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 + num2;
+            Assert.AreEqual(TestAddBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void SumBigTest2()
+        {
+            string n1 = "111111111111111111111111145454567";
+            string n2 = "-1";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 + num2;
+            Assert.AreEqual(TestAddBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
         public void SubBigTest()
+        {
+            string n2 = "885495849589458945849584958489588";
+            string n1 = "111111111111111111111111145454567";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 - num2;
+            Assert.AreEqual(TestMinusBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void SubBigTest1()
         {
             string n2 = "111111111111111111111111145454567";
             string n1 = "885495849589458945849584958489588";
@@ -37,7 +97,37 @@ namespace Tests
             Assert.AreEqual(TestMinusBig(n1, n2), result.ToString());
         }
         [TestMethod]
+        public void SubBigTest2()
+        {
+            string n2 = "111111111111111111111111145454567";
+            string n1 = "111111111111111111111111145454567";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 - num2;
+            Assert.AreEqual(TestMinusBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void SubBigTest3()
+        {
+            string n2 = "111111111111111111111111145454567";
+            string n1 = "0";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 - num2;
+            Assert.AreEqual(TestMinusBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
         public void DivBigTest()
+        {
+            string n1 = "111111111111111111111111145454567";
+            string n2 = "111111111111111111111111145454567";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 / num2;
+            Assert.AreEqual(TestDivideBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void DivBigTest1()
         {
             string n1 = "111111111111111111111111145454567";
             string n2 = "885495849589458945849584958489588";
@@ -47,7 +137,37 @@ namespace Tests
             Assert.AreEqual(TestDivideBig(n1, n2), result.ToString());
         }
         [TestMethod]
-        public void OstBigTest()
+        public void DivBigTest2()
+        {
+            string n1 = "0";
+            string n2 = "885495849589458945849584958489588";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 / num2;
+            Assert.AreEqual(TestDivideBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void DivBigTest3()
+        {
+            string n1 = "885495849589458945849584958489588";
+            string n2 = "1";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 / num2;
+            Assert.AreEqual(TestDivideBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void DivBigTest4()
+        {
+            string n1 = "885495849589458945849584958489588";
+            string n2 = "0";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 / num2;
+            Assert.AreEqual(TestDivideBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void OstBigTest1()
         {
             string n1 = "111111111111111111111111145454567";
             string n2 = "885495849589458945849584958489588";
@@ -57,10 +177,70 @@ namespace Tests
             Assert.AreEqual(TestOstBig(n1, n2), result.ToString());
         }
         [TestMethod]
-        public void PowBigTest()
+        public void OstBigTest2()
+        {
+            string n1 = "111111111111111111111111145454567";
+            string n2 = "111111111111111111111111145454567";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 % num2;
+            Assert.AreEqual(TestOstBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void OstBigTest3()
+        {
+            string n1 = "111111111111111111111111145454567";
+            string n2 = "1";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 % num2;
+            Assert.AreEqual(TestOstBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void OstBigTest4()
+        {
+            string n1 = "111111111111111111111111145454567";
+            string n2 = "0";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 % num2;
+            Assert.AreEqual(TestOstBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void OstBigTest5()
+        {
+            string n1 = "0";
+            string n2 = "111111111111111111111111145454567";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 % num2;
+            Assert.AreEqual(TestOstBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void PowBigTest1()
         {
             string n1 = "1234567891234567891234689";
             string n2 = "756";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 ^ num2;
+            Assert.AreEqual(TestPowBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void PowBigTest2()
+        {
+            string n1 = "1234567891234567891234689";
+            string n2 = "1";
+            BigInt num1 = new BigInt(n1);
+            BigInt num2 = new BigInt(n2);
+            BigInt result = num1 ^ num2;
+            Assert.AreEqual(TestPowBig(n1, n2), result.ToString());
+        }
+        [TestMethod]
+        public void PowBigTest3()
+        {
+            string n1 = "1234567891234567891234689";
+            string n2 = "0";
             BigInt num1 = new BigInt(n1);
             BigInt num2 = new BigInt(n2);
             BigInt result = num1 ^ num2;
